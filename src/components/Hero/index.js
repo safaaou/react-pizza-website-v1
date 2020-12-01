24eda957cd5b24ac1af2b276ec07e57fec1f17b0
safaaou <safaa.ouaazzan@gmail.com>
2020-12-01 08:08:45 +0100

    first commit

---
+++ src/components/Hero/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import NavBar from "../NavBar";
+import Sidebar from "../Sidebar";
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HeroElements";
+
+export const Hero = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <HeroContainer>
+      <NavBar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Greatest Pizza Ever</HeroH1>
+          <HeroP>Ready in 60 seconds</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
